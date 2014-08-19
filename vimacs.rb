69b95b416a37b8e74677bfc8517dbfc456a48bd8
CodeHex <codehex@AirBook.local>
2014-08-19 14:34:27 +0900

    vimacs
    ruby 始めたからついでに。
    GUIならSwiftがいいね

--- vimacs.rb
+++ vimacs.rb
@@ -1,19 +1,45 @@
-### ebox.rb
-Shoes.app :title => "vimacs", :width => 200, :height =>  180 do
-    stack do
-        flow do
-            button "Save" do
-#                save_as = ask_save_file
-                handle = open("noname.text", 'w')
-                handle.write(@e.text)
-                handle.close
+require 'rubygems'
+require 'green_shoes'
+# coding : utf-8
+
+    Shoes.app title: "vimacs", width: 500, height: 400  do
+            flow do
+                Edit_line = edit_line width: 340
+                button "Open" do
+                    Chose_files = ask_open_file
+                    Edit.text = File.read(Chose_files)
+                    Edit_line.text = Chose_files
+                end
+
+                button "Indent" do
+                    gocha_txt = Edit.text
+                    lines = gocha_txt.split $/
+                    lines.collect! { |line| '  ' + line }
+                    Edit.text = lines.join $/
+                end
+
+                button "Clear" do
+                    gocha_txt = Edit.text
+                    clear_lines = gocha_txt.split $/
+                    clear_lines.collect! { |line| '' }
+                    Edit.text = clear_lines.join
+                end
+
+                stack margin: 5 do
+
+                    Edit = edit_box width: '100%', height: 300, font: "osaka-mono"
+                    button "Save", width: '100%' do
+                    File.open(Chose_files, 'wb') do |files|
+                        files.write Edit.text
+                end
             end
-            button "Open" do
-                f = ask_open_file
-                @e.text = File.read(f)
+                    button "SaveAs", width: '100%' do
+                    file = ask_save_file
+                    File.open(file, 'wb') do |files|
+                    files.write Edit.text
+                end
             end
+
         end
-        @e = edit_box
     end
 end
-          
